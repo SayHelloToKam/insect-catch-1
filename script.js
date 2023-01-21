df57d7630804ee00539f0c19e4f273104f594611
SayHelloToKam <sayhello@kamilwatson.com>
2023-01-20 23:52:55 -0500

    add some game functionality

--- script.js
+++ script.js
@@ -17,3 +17,32 @@ let score = 0
 let selected_insect = {}
 
 start_btn.addEventListener('click', ()=> screens[0].classList.add('up'))
+
+choose_insect_btns.forEach(btn=> {
+    btn.addEventListener('click', () => {
+        const img = btn.querySelector('img')
+        const src = img.getAttribute('src')
+        const alt = img.getAttribute('alt')
+        selected_insect = { src, alt }
+        screen[1].classList.add('up')
+        setTimeout(createInsect, 1000)
+        startGame()
+    })
+})
+
+function createInsect() {
+    const insect = document.createElement('div')
+    insect.classList.add('insect')
+    const { XMLDocument, y } = getRandomLocation()
+    insect.style.top = `${y}px`
+    insect.style.left = `${x}px`
+    insect.innerHTML = `<img src="${selected_insect.src}" alt="${selected_insect.alt}" />`
+}
+
+function getRandowLocation() {
+    const width = window.innerWidth
+    const height = window.innerHeight
+    const x = Math.random() * (width - 200) + 100
+    const y = Math.random() * (height - 200) + 100
+    return { x, y }
+}
